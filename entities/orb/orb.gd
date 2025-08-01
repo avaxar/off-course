cfaa5089891790918106432abdfcf4057f91205d
Avaxar <44055981+avaxar@users.noreply.github.com>
2025-08-01 16:34:00 +0700

    Initial commit

---
+++ entities/orb/orb.gd
@@ -0,0 +1,56 @@
+class_name Orb
+extends RigidBody2D
+
+@export var active: bool = true
+@export var intensity: float = 1.0
+@export var correction_radius: float = 64.0
+@export var preferred_radius: float = 32.0
+
+func _ready() -> void:
+	pass
+
+func _process(_delta: float) -> void:
+	pass
+
+func _physics_process(_delta: float) -> void:
+	if active:
+		apply_force(gravitate())
+
+func gravitate(exclusions: Array = []) -> Vector2:
+	var force := Vector2(0.0, 0.0)
+	for orb: Orb in get_tree().get_nodes_in_group("orbs"):
+		if orb == self or not orb.active or orb in exclusions:
+			continue
+
+		var g := 1000000.0 * orb.intensity
+
+		# Newton's universal gravitation
+		var distance_sq := (orb.global_position - global_position).length_squared()
+		var distance := sqrt(distance_sq)
+		var direction := (orb.global_position - global_position) / distance
+		var force_vec := g * mass * orb.mass / distance_sq * direction
+		force += force_vec
+
+		# Circular orbit correction
+		if distance > correction_radius:
+			continue
+
+		var perpendicular := direction.rotated(PI / 2.0)
+		if linear_velocity.normalized().dot(perpendicular) < 0.0:
+			# Makes sure the perpendicular direction matches with the direction
+			perpendicular = -perpendicular
+		var orbital_speed := sqrt(g * orb.mass / distance) * mass
+		var orbital_vec := perpendicular * orbital_speed
+
+		var correction_vec := orbital_vec - linear_velocity
+		var correction_strength := 1.0 - (distance / correction_radius) ** 64.0
+		force += correction_vec * correction_strength * mass
+
+		# Preferred orbital radius, as to not hit directly
+		if distance > preferred_radius:
+			continue
+		
+		var safety_strength := 1.0 - (distance / preferred_radius) ** 4.0
+		force += -force_vec * safety_strength
+	
+	return force
---
+++ entities/player/player.gd
@@ -0,0 +1,53 @@
+class_name Player
+extends Orb
+
+@export var trajectory_steps: int = 200
+@export var trajectory_step_size: float = 1.0 / 60.0
+
+@onready var sprites: Node2D = $Sprites
+@onready var trajectory_probe: Orb = $TrajectoryProbe
+@onready var trajectory_line: Line2D = $TrajectoryLine
+@onready var gravitated_trajectory_line: Line2D = $GravitatedTrajectoryLine
+
+var latched := false
+var dead := false
+
+func _process(_delta: float) -> void:
+	latched = not dead and Input.is_action_pressed("latch")
+	draw_trajectories()
+
+func gravitate(exclusions: Array = []) -> Vector2:
+	if latched:
+		return super(exclusions + [trajectory_probe])
+	else:
+		return Vector2(0.0, 0.0)
+
+func map_trajectory(gravitated: bool) -> PackedVector2Array:
+	trajectory_probe.position = Vector2(0.0, 0.0)
+	trajectory_probe.mass = mass
+	trajectory_probe.linear_velocity = linear_velocity
+
+	var path: PackedVector2Array = [Vector2(0.0, 0.0)]
+	for _i in range(trajectory_steps):
+		trajectory_probe.position += trajectory_probe.linear_velocity * trajectory_step_size
+		if gravitated:
+			var accel := trajectory_probe.gravitate([self]) / trajectory_probe.mass
+			trajectory_probe.linear_velocity += accel * trajectory_step_size
+
+		var obstructed := false
+		for orb: Orb in get_tree().get_nodes_in_group("orbs"):
+			if orb == self or not orb.active:
+				continue
+			if (orb.global_position - trajectory_probe.global_position).length() <= 16:
+				obstructed = true
+				break
+		if obstructed:
+			break
+
+		path.append(trajectory_probe.position)
+
+	return path
+
+func draw_trajectories() -> void:
+	trajectory_line.points = map_trajectory(false)
+	gravitated_trajectory_line.points = map_trajectory(true)
