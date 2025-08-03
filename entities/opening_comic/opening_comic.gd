ce89d2cd40893be383f8495788917a8498a9c59b
Troggz <thomasnadandraz@gmail.com>
2025-08-03 17:52:03 +0700

    3
    121

---
+++ entities/opening_comic/opening_comic.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+var panel:int = 0
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("latch") && panel == 0:
+		$AnimationPlayer.play("panel_1")
+		print("com")
+		
+	if Input.is_action_just_pressed("latch") && panel == 1:
+		$AnimationPlayer.play("panel_2")
+		print("com")
+	if Input.is_action_just_pressed("latch") && panel == 2:
+		$AnimationPlayer.play("panel_3")
+		print("com")
+	if Input.is_action_just_pressed("latch") && panel == 3:
+		$AnimationPlayer.play("panel_4")
+		print("com")
+	
+	if Input.is_action_just_pressed("latch") && panel == 4:
+		get_tree().change_scene_to_file("res://levels/level_0.tscn")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	panel += 1
