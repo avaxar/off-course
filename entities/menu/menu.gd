286125e9ed89a92682eb6e6da3d570bdf443b9a6
Avaxar <44055981+avaxar@users.noreply.github.com>
2025-08-03 23:30:40 +0700

    fade out music

--- entities/menu/menu.gd
+++ entities/menu/menu.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
 
+var closing := false
 func _process(_delta: float) -> void:
+	if closing:
+		$Music.volume_db -= 100.0 * _delta
+
 	if Input.is_action_just_pressed("latch"):
 		$StartButton.play("hold")
 	if Input.is_action_just_released("latch"):
 		$CanvasLayer/Transition.play("close")
+		closing = true
 
 
 func _on_transition_animation_finished() -> void:
